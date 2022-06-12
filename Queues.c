798529f90f72e1fd00266f03beec8ca9e2bfe0b2
abdulahe <abdulah_eminovic@outlook.com>
2022-06-12 03:00:30 +0200

    all

--- Queues.c
+++ Queues.c
@@ -59,3 +59,76 @@
 //     printf("pop: %d", pop());
 //     return 0;
 // }
+
+// queues with linked lists
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *link;
+} NODE;
+
+NODE *front = NULL, *rear = NULL;
+
+void PUSH(int elem);
+int POP();
+void DISPLAY();
+
+void PUSH(int elem)
+{
+    NODE *p;
+    p = (NODE *)malloc(sizeof(NODE));
+    p->data = elem;
+    p->link = NULL;
+    if (rear == NULL)
+        front = rear = p;
+    else
+    {
+        rear->link = p;
+        rear = p;
+    }
+}
+
+int POP()
+{
+    NODE *p;
+    if (front == NULL)
+        printf("UNDERFLOW!!");
+    int elem = front->data;
+    if (front == rear)
+        rear = NULL;
+    p = front;
+    front = front->link;
+    p->link = NULL;
+    free(p);
+    return elem;
+}
+
+void DISPLAY()
+{
+    NODE *p;
+    printf("front->");
+    p = front;
+    while (p != rear)
+    {
+        printf("|%d", p->data);
+        p = p->link;
+    }
+    printf("|%d|<-rear\n\n", rear->data);
+}
+
+int main()
+{
+    PUSH(5);
+    PUSH(1);
+    PUSH(3);
+    PUSH(2);
+    PUSH(4);
+    DISPLAY();
+    printf("POP: %d\n\n", POP());
+    DISPLAY();
+    return 0;
+}
